5e16b4e0a5b12df7ccb90ede9b8627afd2f7feec
Michel Melo <michelmelosj25@hotmail.com>
2020-10-14 19:53:59 -0300

    criacao metodos delete e update, correcao na criacao e index

--- backend/src/controllers/UserController.js
+++ backend/src/controllers/UserController.js
@@ -1,16 +1,17 @@
 const User = require('../model/User');
 const Room = require('../model/Room');
+const { update } = require('../model/User');
 
 module.exports = {
     async index(request, response) {
-        const data = await User.find({});
-
+        const data = await User.find();
         return response.json(data);
     },
 
     async create(request, response) {
         const {nome,email,password} = request.body;
-        const morador = await User.find({email});
+        const morador = await User.findOne({'email':email});
+
         if(morador){
             return response.json({message:"email já existe"});
         }
@@ -22,10 +23,35 @@ module.exports = {
     },
 
     async delete(request, response) {
-        return response.json({message:"ok"});
+        const user_id = request.headers.authorization;
+
+        const result = await User.remove({'_id': user_id});
+
+        if(result.deletedCount > 0){
+            return response.json("Usuario removido com sucesso!");
+        }
+
+        return response.json("Erro ao remover usuario!");
     },
 
     async put(request, response) {
         return response.json({message:"ok"});
+    },
+
+    async update(request, response) {
+        const {nome,email,password} = request.body;
+        const user_id = request.headers.authorization;
+        const {id} = request.params;
+
+        if(id == user_id){
+            const result = await User.update({'_id': user_id},{$set: {'nome': nome, 'email': email, 'password': password}});
+            if(result.nModified > 0){
+                return response.json("Atualização realizada com sucesso");
+            }else{
+                return response.json("Usuario não cadastrado");
+            }
+        }
+
+        return response.json("Operação não altorizada");
     }
 }
