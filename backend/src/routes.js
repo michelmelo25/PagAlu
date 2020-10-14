0434a92213969e32400b08a99f47e8390fb8e36e
Michel Melo <michelmelosj25@hotmail.com>
2020-10-14 14:51:21 -0300

    criacao das novas rotas

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -12,5 +12,8 @@ routes.get('/users', Authentication.verifyJWT, UserController.index);
 routes.post('/login', Authentication.login);
 routes.post('/logout', Authentication.logout);
 
+routes.get('/rooms', RoomController.index);
+routes.post('/room', RoomController.create);
+routes.post('/room/:id', RoomController.add_membro);
 
 module.exports = routes;
