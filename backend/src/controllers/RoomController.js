c07f1c0be9d6f6b135eee948c628f8c8e9771092
Michel Melo <michelmelosj25@hotmail.com>
2020-10-14 14:50:49 -0300

    metodos de index, criacao e add novo membro em uma room

--- backend/src/controllers/RoomController.js
+++ backend/src/controllers/RoomController.js
@@ -1,16 +1,43 @@
 const crypto = require('crypto');
+const { update } = require('../model/Room');
+const Room = require('../model/Room');
 
 module.exports = {
     async index(request, response) {
-        return({message:"ok"});
+        const data = await Room.find({});
+
+        return response.json(data);
     },
 
     async create(request, response) {
-        return({message:"ok"});
+        const {nome} = request.body;
+        const user_id = request.headers.authorization;
+
+        const room = {
+            'nome': nome,
+            'adm': user_id,
+            'membros': [user_id]
+        }
+        console.log(room);
+        const {_id} = await Room.create(room);
+        console.log(_id);
+
+        return response.json({_id});
     },
 
-    async add(request, response){
-        return({message:"ok"});
+    async add_membro(request, response){
+        const {user_id} = request.body;
+        const {id} = request.params;
+        const adm_id = request.headers.authorization;
+
+        const room = await Room.findById(id);
+        
+        if(room.adm === adm_id){
+            const update = await Room.update({'_id': id},{$push:{'membros': user_id}});
+            console.log(update);
+        }
+
+        return response.json(room);
     },
 
     async delete(request, response){
