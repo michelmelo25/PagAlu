4b0338956416ff366ebb7a428a0594e17b833843
Michel Melo <michelmelosj25@hotmail.com>
2020-10-14 19:54:43 -0300

    criacao rotas update e delete

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -7,7 +7,9 @@ const Authentication = require('./controllers/Authentication');
 const routes = express.Router();
 
 routes.post('/register', UserController.create);
-routes.get('/users', Authentication.verifyJWT, UserController.index);
+routes.get('/users', UserController.index); //, Authentication.verifyJWT
+routes.delete('/user', UserController.delete);
+routes.put('/user/edit/:id', UserController.update);
 
 routes.post('/login', Authentication.login);
 routes.post('/logout', Authentication.logout);
