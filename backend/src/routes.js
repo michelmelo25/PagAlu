3f371f77d49f86180cef5f35a97382e5259ce545
MarceloRavache <jm.ravache@outlook.com>
2020-10-14 15:06:38 -0300

    bug fix

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,7 +3,6 @@ const express = require('express');
 const UserController = require('./controllers/UserController');
 const RoomController = require('./controllers/RoomController');
 const Authentication = require('./controllers/Authentication');
-const AuthenticationAdmin = require('./controllers/AuthenticationAdmin');
 
 const routes = express.Router();
 
@@ -13,11 +12,7 @@ routes.get('/users', Authentication.verifyJWT, UserController.index);
 routes.post('/login', Authentication.login);
 routes.post('/logout', Authentication.logout);
 
-routes.post('/admin/login', AuthenticationAdmin.login);
-routes.post('/admin/logout', AuthenticationAdmin.logout);
-routes.post('/admin/register', UserController.create);
-
-routes.get('/room', AuthenticationAdmin.verifyJWT, RoomController.index);
-routes.post('/room', AuthenticationAdmin.verifyJWT, RoomController.create);
+routes.get('/room', Authentication.verifyJWT, RoomController.index);
+routes.post('/room', Authentication.verifyJWT, RoomController.create);
 
 module.exports = routes;
